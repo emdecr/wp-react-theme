c8c37f7c653a4fe4a6e576c8610f7bd8e5e5c3f7
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-03-12 20:19:05 -0400

    Sets workable base

---
+++ wp-api/src/components/Home.js
@@ -0,0 +1,18 @@
+import React    from 'react';
+import DataStore    from './../stores/DataStore.js';
+
+class Home extends React.Component {
+
+    render() {
+    	let allData = DataStore.getAll();
+        console.log(allData); 
+
+        return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
